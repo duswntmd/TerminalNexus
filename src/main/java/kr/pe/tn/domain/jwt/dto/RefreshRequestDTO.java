90885a4b020bbae679f6b465a9ffc8847f27e2ad
Juseung Yeon <wnend1010@naver.com>
2025-11-02 17:55:50 +0900

    백엔드 : Jwt + OAuth2 업데이트

---
+++ src/main/java/kr/pe/tn/domain/jwt/dto/RefreshRequestDTO.java
@@ -0,0 +1,13 @@
+package kr.pe.tn.domain.jwt.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RefreshRequestDTO {
+
+    @NotBlank
+    private String refreshToken;
+}
--- src/main/java/kr/pe/tn/domain/user/repository/UserRepository.java
+++ src/main/java/kr/pe/tn/domain/user/repository/UserRepository.java
@@ -1,22 +1,20 @@
 package kr.pe.tn.domain.user.repository;
 
-import jakarta.transaction.Transactional;
 import kr.pe.tn.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Boolean existsByUsername(String username);
 
-//    Boolean existsByNickname(String nickname);
-
-    Boolean existsByEmail(String email);
-
-    //username을 받아 DB 테이블에서 회원을 조회하는 메소드 작성
-    UserEntity findByUsername(String username);
-
-    public boolean existsByNickname(String nickname);
+    Optional<UserEntity> findByUsernameAndIsLockAndIsSocial(String username, Boolean isLock, Boolean isSocial);
+    Optional<UserEntity> findByUsernameAndIsLock(String username, Boolean isLock);
+    Optional<UserEntity> findByUsernameAndIsSocial(String username, Boolean social);
 
     @Transactional
     void deleteByUsername(String username);
+
 }
